98591854d806b733df1b4a8cb202271ec0ab53b1
TonyYang9527 <leetony554@gmail.com>
2017-05-07 11:15:04 +0800

    code add

---
+++ src/main/java/com/cell/user/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.cell.user.mapper;
+
+import com.cell.user.entiy.User;
+
+/**
+ * Created by zl on 2015/8/27.
+ */
+public interface UserMapper {
+    public User findUserInfo();
+}
---
+++ src/main/java/com/cell/user/service/UserService.java
@@ -0,0 +1,25 @@
+package com.cell.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cell.user.entiy.User;
+import com.cell.user.mapper.UserMapper;
+
+/**
+ * Created by zl on 2015/8/27.
+ */
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User getUserInfo(){
+        User user=userMapper.findUserInfo();
+        //User user=null;
+        return user;
+    }
+
+}
